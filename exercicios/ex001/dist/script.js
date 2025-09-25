35618ab15b184fd24f227c774ca66bc67a5629c8
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-09-25 15:52:47 -0300

    Initial commit

---
+++ exercicios/ex001/dist/script.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//tipos basicos
+let nome = "Leonardo"; //variavel do tipo string
+let idade = 30; //variavel do tipo number
+let isValid = true; //variavel do tipo boolean
+let todos = "teste"; //variavel que pode receber qualquer tipagem
+//arrays
+let numeros = [1, 2, 3]; //array de números
+let nomes = ["Leonardo", "João", "Fernando"]; //array de strings
+let booleanos = [true, false, true]; //array de booleanos
+let arrayDeTudo = [1, "teste", true]; //array de qualquer tipagem
---
+++ exercicios/ex001/src/script.ts
@@ -0,0 +1,10 @@
+//tipos basicos
+let nome: string = "Leonardo"; //variavel do tipo string
+let idade: number = 30; //variavel do tipo number
+let isValid: boolean = true; //variavel do tipo boolean
+let todos: any = "teste"; //variavel que pode receber qualquer tipagem
+//arrays
+let numeros: number[] = [1, 2, 3]; //array de números
+let nomes: string[] = ["Leonardo", "João", "Fernando"]; //array de strings
+let booleanos: boolean[] = [true, false, true]; //array de booleanos
+let arrayDeTudo: any[] = [1, "teste", true]; //array de qualquer tipagem
