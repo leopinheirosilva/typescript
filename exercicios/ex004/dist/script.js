918505436e957ccefeca0595572e064bd267f4a2
leopinheirosilva <leonardopinheirosilva16@gmail.com>
2025-09-25 18:30:53 -0300

    update script.ts

--- exercicios/ex004/dist/script.js
+++ exercicios/ex004/dist/script.js
@@ -1,10 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //enum
 var Direction;
 (function (Direction) {
     Direction[Direction["Up"] = 1] = "Up";
     Direction[Direction["Down"] = 2] = "Down";
+    Direction["Left"] = "Esquerda";
 })(Direction || (Direction = {}));
 const direction = Direction.Up; //valor 1
 console.log(direction);
+export {};
--- exercicios/ex004/src/script.ts
+++ exercicios/ex004/src/script.ts
@@ -2,6 +2,7 @@
 enum Direction {
   Up = 1,
   Down = 2,
+  Left = "Esquerda",
 }
 const direction = Direction.Up; //valor 1
 console.log(direction);
